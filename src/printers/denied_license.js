129c2dcaef85ac0ba3b38f4b0e7f0ee64efdd9ea
Mark <markjhonpaul.quijom@gmail.com>
2019-05-31 16:14:42 +0800

    for multiple denied license

--- src/printers/denied_license.js
+++ src/printers/denied_license.js
@@ -22,75 +22,85 @@ function fillup(details) {
     };
 }
 
-function getContent(details) {
-    var content = [{
-        layout: "noBorders",
-        table: {
-            widths: [23, '*'],
-            heights: [182, 31, 31, 13, 45, 44, 113],
-            body: [
-                ["", ""], //182
-                ["", { //31
-                    text: details.date_created,
-                    bold: true,
-                    fontSize: 12,
-                }],
-                ["", { //31
-                    text: details.name,
-                    bold: true,
-                    fontSize: 12,
-                }],
-                ["", { //13
-                    text: details.establishment_name,
-                    bold: true,
-                    fontSize: 12,
-                }],
-                ["", { //45
-                    text: details.establishment_address,
-                    bold: true,
-                    fontSize: 12,
-                }],
-                ["", { //44
-                    layout: "noBorders",
-                    table: {
-                        widths: [21, '*'],
-                        body: [
-                            ["", {
-                                text: details.name,
-                                bold: true,
-                                fontSize: 12,
-                            }]
-                        ]
-                    }
-                }],
-                ["", { //113
-                    layout: "noBorders",
-                    table: {
-                        widths: [158, 113, 94, '*'],
-                        body: [
-                            ["",
-                                {
-                                    text: details.application_type,
-                                    fontSize: 12,
-                                    bold: true,
-                                    alignment: "center"
-                                }, "", {
-                                    text: details.case_no,
+function getContent(forms) {
+    var _forms = Array.isArray(forms) ? forms : [forms];
+    var content = [];
+  
+    _forms.forEach(details => {
+        content.push({
+          pageBreak: content.length >= 4 ? 'before' : '',
+          text: "",
+          fontSize: 11
+        });
+        content.push({
+            layout: "noBorders",
+            table: {
+                widths: [23, '*'],
+                heights: [182, 31, 31, 13, 45, 44, 113],
+                body: [
+                    ["", ""], //182
+                    ["", { //31
+                        text: details.date_created,
+                        bold: true,
+                        fontSize: 12,
+                    }],
+                    ["", { //31
+                        text: details.name,
+                        bold: true,
+                        fontSize: 12,
+                    }],
+                    ["", { //13
+                        text: details.establishment_name,
+                        bold: true,
+                        fontSize: 12,
+                    }],
+                    ["", { //45
+                        text: details.establishment_address,
+                        bold: true,
+                        fontSize: 12,
+                    }],
+                    ["", { //44
+                        layout: "noBorders",
+                        table: {
+                            widths: [21, '*'],
+                            body: [
+                                ["", {
+                                    text: details.name,
                                     bold: true,
                                     fontSize: 12,
-                                }
+                                }]
                             ]
-                        ]
-                    }
-                }],
-                ["", {
-                    text: details.reasons,
-                    italics: true,
-                    fontSize: 12,
-                }]
-            ]
-        }
-    }];
+                        }
+                    }],
+                    ["", { //113
+                        layout: "noBorders",
+                        table: {
+                            widths: [158, 113, 94, '*'],
+                            body: [
+                                ["",
+                                    {
+                                        text: details.application_type,
+                                        fontSize: 12,
+                                        bold: true,
+                                        alignment: "center"
+                                    }, "", {
+                                        text: details.case_no,
+                                        bold: true,
+                                        fontSize: 12,
+                                    }
+                                ]
+                            ]
+                        }
+                    }],
+                    ["", {
+                        text: details.reasons,
+                        italics: true,
+                        fontSize: 12,
+                    }]
+                ]
+            }
+        })
+    })
     return content;
 }
 
